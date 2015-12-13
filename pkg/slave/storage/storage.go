d7e355488b54366f22f98db9dbb72ec519acd4c6
YuYang <yy2012cn@gmail.com>
2015-12-13 22:18:56 +0800

    storage interface

--- pkg/slave/storage/storage.go
+++ pkg/slave/storage/storage.go
@@ -1,7 +1,12 @@
 package storage
 
+// import (
+// 	"github.com/syndtr/goleveldb/leveldb"
+// )
+
 type Storage interface {
-  Get() string
-  Put() string
-  
+	Get(key []byte, options interface{}) (val []byte, err error)
+	Put(key []byte, val []byte, options interface{}) error
+	Delete(key []byte, options interface{}) error
+	Close() error
 }
